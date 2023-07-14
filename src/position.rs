3a1afbc75b9437c9af5d47b4ab507ef13b17bdca
Carsten Wenderdel <public@wenderdel.com>
2023-07-14 22:25:01 +0200

    Refactor: Use 26 element array for pips
    This way, the position within the array exactly match the numbers normally used for everything -> less headache.

--- src/position.rs
+++ src/position.rs
@@ -2,15 +2,12 @@
 /// We assume two players "x" and "o".
 #[derive(Debug, PartialEq)]
 struct Position {
-    pips: [i8; 24],
-    x_out: OutCheckers,
-    o_out: OutCheckers,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-struct OutCheckers {
-    bar: u8,
-    off: u8,
+    // Array positions 25 and 0 are the bar.
+    // The other array positions are the pips from the point of view of x, moving from 24 to 0.
+    // A positive number means x has that many checkers on that point. Negative for o.
+    pips: [i8; 26],
+    x_off: u8,
+    o_off: u8,
 }
 
 impl Position {
@@ -20,35 +17,36 @@ impl Position {
         pips.reverse();
         Position {
             pips,
-            x_out: self.o_out,
-            o_out: self.x_out,
+            x_off: self.o_off,
+            o_off: self.x_off,
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::position::{OutCheckers, Position};
+    use crate::position::Position;
 
     #[test]
     fn switch_sides() {
         // Given
         let original = Position {
             pips: [
-                2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, -2, 0, -2, 0, -2, 0, -2, 0, -2, 0, -2,
+                2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, -2, 0, -2, 0, -2, 0, -2, 0, -2, 0, -2, 0,
             ],
-            x_out: OutCheckers { bar: 2, off: 0 },
-            o_out: OutCheckers { bar: 0, off: 3 },
+            x_off: 0,
+            o_off: 3,
         };
         // When
         let actual = original.switch_sides();
         // Then
         let expected = Position {
             pips: [
-                2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,
+                0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                -2, -2,
             ],
-            x_out: OutCheckers { bar: 0, off: 3 },
-            o_out: OutCheckers { bar: 2, off: 0 },
+            x_off: 3,
+            o_off: 0,
         };
         assert_eq!(actual, expected);
     }
