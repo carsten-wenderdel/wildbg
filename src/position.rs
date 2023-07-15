5cc7694f25bb30b1064c93ea583045e7962b9f27
Carsten Wenderdel <public@wenderdel.com>
2023-07-15 12:01:25 +0200

    Implement fmt::Debug for Position

--- src/position.rs
+++ src/position.rs
@@ -1,6 +1,10 @@
+use std::fmt;
+use std::fmt::Formatter;
+use std::fmt::Write;
+
 /// A single position in backgammon without match information.
 /// We assume two players "x" and "o".
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 struct Position {
     // Array positions 25 and 0 are the bar.
     // The other array positions are the pips from the point of view of x, moving from 24 to 0.
@@ -20,6 +24,48 @@ impl Position {
             x_off: self.o_off,
             o_off: self.x_off,
         }
+    }
+}
+
+impl fmt::Debug for Position {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Position:").unwrap();
+
+        // Write x:
+        let mut s = String::from("x: {");
+        if self.pips[0] > 0 {
+            write!(s, "bar:{}, ", self.pips[0]).unwrap();
+        }
+        for i in (1..25).rev() {
+            if self.pips[i] > 0 {
+                write!(s, "{}:{}, ", i, self.pips[i]).unwrap();
+            }
+        }
+        if self.x_off > 0 {
+            write!(s, "off:{}, ", self.x_off).unwrap();
+        }
+        s.pop(); // remove last ", "
+        s.pop();
+        writeln!(s, "}}").unwrap();
+        write!(f, "{}", s).unwrap();
+
+        // Write o:
+        let mut s = String::from("o: {");
+        if self.o_off > 0 {
+            write!(s, "off:{}, ", self.x_off).unwrap();
+        }
+        for i in (1..25).rev() {
+            if self.pips[i] < 0 {
+                write!(s, "{}:{}, ", i, -self.pips[i]).unwrap();
+            }
+        }
+        if self.pips[25] < 0 {
+            write!(s, "bar:{}, ", -self.pips[25]).unwrap();
+        }
+        s.pop(); // remove last ", "
+        s.pop();
+        write!(s, "}}").unwrap();
+        write!(f, "{}", s)
     }
 }
 
