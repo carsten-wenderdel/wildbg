894fd8dbca50bc9c212b4962b4513a44e84e8ec4
Carsten Wenderdel <public@wenderdel.com>
2023-07-23 19:46:10 +0200

    Implement regular move with no checkers on bar

--- src/position/regular_moves.rs
+++ src/position/regular_moves.rs
@@ -1,18 +1,115 @@
 use crate::position::{Position, O_BAR, X_BAR};
+use std::cmp::max;
 
 impl Position {
+    /// Returns a vector of all possible moves when rolling a double.
+    /// The return value both contains the moves and the resulting positions.
+    /// The move is encoded in an array of 2 numbers, representing the pip from where to move.
+    /// Element '0' in that array is the pip from where to move with the first die,
+    /// element '1' is the pip from where to move with the second die.
     #[allow(dead_code)]
     fn all_regular_moves(&self, die1: usize, die2: usize) -> Vec<([Option<usize>; 2], Position)> {
         debug_assert!(die1 > die2);
         match self.pips[X_BAR] {
-            0 => Vec::new(),
+            0 => self.moves_with_0_checkers_on_bar(die1, die2),
             1 => Vec::new(),
-            _ => self.enter_2_move(die1, die2),
+            _ => self.moves_with_2_checkers_on_bar(die1, die2),
         }
     }
 
+    /// Regular moves with no checkers on the bar.
+    fn moves_with_0_checkers_on_bar(
+        &self,
+        die1: usize,
+        die2: usize,
+    ) -> Vec<([Option<usize>; 2], Position)> {
+        debug_assert!(die1 > die2);
+        debug_assert!(self.pips[X_BAR] == 0);
+
+        match self.move_possibilities(die1, die2) {
+            MovePossibilities::None => Vec::from([([None, None], self.clone())]),
+            MovePossibilities::One { die } => {
+                let index = if die == die1 { 0 } else { 1 };
+                self.one_checker_moves(die, index, None)
+            }
+            MovePossibilities::Two => self.two_checker_moves(die1, die2),
+        }
+    }
+
+    fn one_checker_moves(
+        &self,
+        die: usize,
+        index: usize,
+        other_value: Option<usize>,
+    ) -> Vec<([Option<usize>; 2], Position)> {
+        debug_assert!(self.pips[X_BAR] == 0);
+
+        let mut moves: Vec<([Option<usize>; 2], Position)> = Vec::new();
+        for i in (1..X_BAR).rev() {
+            if self.can_move(i, die) {
+                let position = self.clone_and_move_single_checker(i, die);
+                let mut the_move = [other_value, other_value];
+                the_move[index] = Some(i);
+                moves.push((the_move, position));
+            }
+        }
+        debug_assert!(!moves.is_empty());
+        moves
+    }
+
+    // All moves with no checkers on the bar where two checkers can be moved.
+    fn two_checker_moves(&self, die1: usize, die2: usize) -> Vec<([Option<usize>; 2], Position)> {
+        debug_assert!(die1 > die2);
+        debug_assert!(self.pips[X_BAR] == 0);
+
+        let mut moves: Vec<([Option<usize>; 2], Position)> = Vec::new();
+        for i in (1..X_BAR).rev() {
+            // Looking at moves where die1 *can* be used first
+            if self.can_move(i, die1) {
+                let position = self.clone_and_move_single_checker(i, die1);
+                // We have to look at all pips in the home board, in case bearing off just became possible. This is why the 7 appears in the max function.
+                for j in (1..max(7, i + 1)).rev() {
+                    if position.can_move(j, die2) {
+                        let final_position = position.clone_and_move_single_checker(j, die2);
+                        moves.push(([Some(i), Some(j)], final_position));
+                    }
+                }
+            }
+            // Looking at moves where die2 *must* be moved first
+            // This can be because of two reasons:
+            // 1. We make two movements with the same checker, but for die1 it's initially blocked.
+            // 2. We make two movements with different checkers and hit something with the first move, either die1 or die2
+            // 3. After moving die2, we now can bear off with die1, which was illegal before.
+            if self.can_move(i, die2) {
+                let position = self.clone_and_move_single_checker(i, die2);
+                // We have to look at all pips in the home board, in case bearing off just became possible. This is why the 7 appears in the max function.
+                for j in (1..max(7, i + 1)).rev() {
+                    if position.can_move(j, die1) {
+                        // This describes cases 1 and 2:
+                        let two_movements_with_same_checker_and_different_outcome =
+                            j == i - die2 && (self.pips[i - die1] < 0 || self.pips[i - die2] < 0);
+                        // This describes case 3:
+                        let bear_off_was_illegal_but_not_anymore = i > 6 && j <= die1;
+                        if two_movements_with_same_checker_and_different_outcome
+                            || bear_off_was_illegal_but_not_anymore
+                        {
+                            let final_position = position.clone_and_move_single_checker(j, die1);
+                            moves.push(([Some(j), Some(i)], final_position));
+                        }
+                    }
+                }
+            }
+        }
+        debug_assert!(!moves.is_empty());
+        moves
+    }
+
     /// All moves (well, exactly one) when at least two checkers are on the bar.
-    fn enter_2_move(&self, die1: usize, die2: usize) -> Vec<([Option<usize>; 2], Position)> {
+    fn moves_with_2_checkers_on_bar(
+        &self,
+        die1: usize,
+        die2: usize,
+    ) -> Vec<([Option<usize>; 2], Position)> {
         debug_assert!(die1 > die2);
         debug_assert!(self.pips[X_BAR] > 1);
 
@@ -28,6 +125,57 @@ impl Position {
             the_move[1] = Some(X_BAR);
         }
         Vec::from([(the_move, position)])
+    }
+
+    /// Will return 2 if 2 or more checkers can be moved.
+    /// Will return 0 if no checker can be moved.
+    fn move_possibilities(&self, die1: usize, die2: usize) -> MovePossibilities {
+        debug_assert!(die1 > die2);
+        debug_assert!(self.pips[X_BAR] == 0);
+
+        let mut can_move_die1 = false;
+        let mut can_move_die2 = false;
+
+        // Move die1 first
+        for i in (1..X_BAR).rev() {
+            if self.can_move(i, die1) {
+                can_move_die1 = true;
+                let position = self.clone_and_move_single_checker(i, die1);
+                // We have to look at all pips in the home board, in case bearing off just became possible. This is why the 7 appears in the max function.
+                for j in (1..max(7, i + 1)).rev() {
+                    if position.can_move(j, die2) {
+                        return MovePossibilities::Two;
+                    }
+                }
+            }
+        }
+
+        // Move die2 first, assuming die1 cannot be moved first
+        for i in (1..X_BAR).rev() {
+            if self.can_move(i, die2) {
+                can_move_die2 = true;
+                let position = self.clone_and_move_single_checker(i, die2);
+                // If die1 and die2 could be used with different checkers without bearing off, then we would not get here.
+                // So, we only need to check if die1 can be moved with the same checker as die2.
+                if i > die2 && position.can_move(i - die2, die1) {
+                    return MovePossibilities::Two;
+                }
+                // Now checking bearing off
+                for j in (1..7).rev() {
+                    if position.can_move(j, die1) {
+                        return MovePossibilities::Two;
+                    }
+                }
+            }
+        }
+
+        if can_move_die1 {
+            MovePossibilities::One { die: die1 }
+        } else if can_move_die2 {
+            MovePossibilities::One { die: die2 }
+        } else {
+            MovePossibilities::None
+        }
     }
 
     fn can_enter(&self, die: usize) -> bool {
@@ -59,10 +207,18 @@ impl Position {
     }
 }
 
+enum MovePossibilities {
+    None,
+    One { die: usize },
+    Two,
+}
+
 #[cfg(test)]
 mod tests {
     use crate::position::{Position, O_BAR, X_BAR};
     use std::collections::HashMap;
+
+    // Two checkers on bar
 
     #[test]
     fn cannot_enter_with_two_checkers_on_bar() {
@@ -130,5 +286,189 @@ mod tests {
         );
         assert_eq!(moves.len(), 1);
         assert_eq!(moves, Vec::from([([Some(X_BAR), Some(X_BAR)], expected)]));
+    }
+
+    // No checkers on bar
+
+    #[test]
+    fn cannot_user_either_die() {
+        // Given
+        let position = Position::from(
+            &HashMap::from([(10, 2), (2, 3)]),
+            &HashMap::from([(8, 2), (6, 2)]),
+        );
+        // When
+        let moves = position.all_regular_moves(4, 2);
+        // Then
+        assert_eq!(moves.len(), 1);
+        assert_eq!(moves, Vec::from([([None, None], position)]));
+    }
+
+    #[test]
+    fn forced_only_smaller_die() {
+        // Given
+        let position = Position::from(&HashMap::from([(7, 2)]), &HashMap::from([(2, 2)]));
+        // When
+        let moves = position.all_regular_moves(5, 2);
+        // Then
+        let expected = Position::from(&HashMap::from([(7, 1), (5, 1)]), &HashMap::from([(2, 2)]));
+        assert_eq!(moves, Vec::from([([None, Some(7)], expected)]));
+    }
+
+    #[test]
+    fn forced_smaller_die_first_then_bear_off() {
+        // Given
+        let position = Position::from(&HashMap::from([(8, 1), (4, 3)]), &HashMap::from([(1, 2)]));
+        // When
+        let moves = position.all_regular_moves(4, 3);
+        // Then
+        let expected = Position::from(&HashMap::from([(5, 1), (4, 2)]), &HashMap::from([(1, 2)]));
+        assert_eq!(moves, Vec::from([([Some(4), Some(8)], expected)]));
+    }
+
+    #[test]
+    fn bigger_die_cannot_move_initially() {
+        // Given
+        let position = Position::from(&HashMap::from([(20, 1)]), &HashMap::from([(16, 3)]));
+        // When
+        let moves = position.all_regular_moves(4, 3);
+        // Then
+        let expected = Position::from(&HashMap::from([(13, 1)]), &HashMap::from([(16, 3)]));
+        assert_eq!(moves, Vec::from([([Some(17), Some(20)], expected)]));
+    }
+
+    #[test]
+    fn smaller_first_allows_bear_off() {
+        // Given
+        let position = Position::from(&HashMap::from([(9, 1), (5, 1)]), &HashMap::from([(20, 2)]));
+        // When
+        let moves = position.all_regular_moves(5, 3);
+        // Then
+        let expected1 = (
+            [Some(9), Some(5)],
+            Position::from(&HashMap::from([(4, 1), (2, 1)]), &HashMap::from([(20, 2)])),
+        );
+        let expected2 = (
+            [Some(9), Some(4)],
+            Position::from(&HashMap::from([(5, 1), (1, 1)]), &HashMap::from([(20, 2)])),
+        );
+        let expected3 = (
+            [Some(5), Some(9)],
+            Position::from(&HashMap::from([(6, 1)]), &HashMap::from([(20, 2)])),
+        );
+        assert_eq!(moves, Vec::from([expected1, expected2, expected3]));
+    }
+
+    #[test]
+    fn could_bear_off_but_could_do_other_moves_as_well() {
+        // Given
+        let position = Position::from(
+            &HashMap::from([(5, 2), (4, 3), (3, 1)]),
+            &HashMap::from([(20, 1)]),
+        );
+        // When
+        let moves = position.all_regular_moves(4, 3);
+        // Then
+        let expected1 = (
+            [Some(5), Some(5)],
+            Position::from(
+                &HashMap::from([(4, 3), (3, 1), (2, 1), (1, 1)]),
+                &HashMap::from([(20, 1)]),
+            ),
+        );
+        let expected2 = (
+            [Some(5), Some(4)],
+            Position::from(
+                &HashMap::from([(5, 1), (4, 2), (3, 1), (1, 2)]),
+                &HashMap::from([(20, 1)]),
+            ),
+        );
+        let expected3 = (
+            [Some(5), Some(3)],
+            Position::from(
+                &HashMap::from([(5, 1), (4, 3), (1, 1)]),
+                &HashMap::from([(20, 1)]),
+            ),
+        );
+        let expected4 = (
+            [Some(4), Some(5)],
+            Position::from(
+                &HashMap::from([(5, 1), (4, 2), (3, 1), (2, 1)]),
+                &HashMap::from([(20, 1)]),
+            ),
+        );
+        let expected5 = (
+            [Some(4), Some(4)],
+            Position::from(
+                &HashMap::from([(5, 2), (4, 1), (3, 1), (1, 1)]),
+                &HashMap::from([(20, 1)]),
+            ),
+        );
+        let expected6 = (
+            [Some(4), Some(3)],
+            Position::from(&HashMap::from([(5, 2), (4, 2)]), &HashMap::from([(20, 1)])),
+        );
+        assert_eq!(
+            moves,
+            Vec::from([expected1, expected2, expected3, expected4, expected5, expected6])
+        );
+    }
+
+    #[test]
+    fn only_one_move_if_order_is_not_important() {
+        // Given
+        let position = Position::from(&HashMap::from([(20, 1)]), &HashMap::from([(22, 2)]));
+        // When
+        let moves = position.all_regular_moves(4, 3);
+        // Then
+        let expected = Position::from(&HashMap::from([(13, 1)]), &HashMap::from([(22, 2)]));
+        assert_eq!(moves, Vec::from([([Some(20), Some(16)], expected)]),);
+    }
+
+    #[test]
+    fn only_one_move_in_home_board_if_order_is_not_important() {
+        // Given
+        let position = Position::from(&HashMap::from([(5, 1)]), &HashMap::from([(22, 2)]));
+        // When
+        let moves = position.all_regular_moves(2, 1);
+        // Then
+        let expected = Position::from(&HashMap::from([(2, 1)]), &HashMap::from([(22, 2)]));
+        assert_eq!(moves, Vec::from([([Some(5), Some(3)], expected)]),);
+    }
+
+    #[test]
+    fn two_moves_if_bigger_die_hits_opponent() {
+        // Given
+        let position = Position::from(&HashMap::from([(10, 1)]), &HashMap::from([(6, 1)]));
+        // When
+        let moves = position.all_regular_moves(4, 2);
+        // Then
+        let expected1 = (
+            [Some(10), Some(6)],
+            Position::from(&HashMap::from([(4, 1)]), &HashMap::from([(O_BAR, 1)])),
+        );
+        let expected2 = (
+            [Some(8), Some(10)],
+            Position::from(&HashMap::from([(4, 1)]), &HashMap::from([(6, 1)])),
+        );
+        assert_eq!(moves, Vec::from([expected1, expected2]));
+    }
+
+    #[test]
+    fn two_moves_if_smaller_die_hits_opponent() {
+        // Given
+        let position = Position::from(&HashMap::from([(5, 1)]), &HashMap::from([(4, 1)]));
+        // When
+        let moves = position.all_regular_moves(3, 1);
+        // Then
+        let expected1 = (
+            [Some(5), Some(2)],
+            Position::from(&HashMap::from([(1, 1)]), &HashMap::from([(4, 1)])),
+        );
+        let expected2 = (
+            [Some(4), Some(5)],
+            Position::from(&HashMap::from([(1, 1)]), &HashMap::from([(O_BAR, 1)])),
+        );
+        assert_eq!(moves, Vec::from([expected1, expected2]));
     }
 }
