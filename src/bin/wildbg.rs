70d3f0e453830d52187f97b6d2f1a22cf0324bbe
Carsten Wenderdel <public@wenderdel.com>
2023-09-01 13:23:43 +0200

    Use json response for server errors
    Also introduce unit testing for the web server.

--- src/bin/wildbg.rs
+++ src/bin/wildbg.rs
@@ -1,5 +1,7 @@
-use axum::{routing::get, Router, Server};
+use axum::http::StatusCode;
+use axum::{routing::get, Json, Router, Server};
 use hyper::Error;
+use serde::Serialize;
 use std::net::{Ipv4Addr, SocketAddr};
 
 #[tokio::main]
@@ -7,19 +9,61 @@ async fn main() -> Result<(), Error> {
     println!("You can access the server via:");
     println!("http://localhost:8080/move");
 
-    let app = Router::new().route("/move", get(axum_bg::get_move));
     let address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8080));
-    Server::bind(&address).serve(app.into_make_service()).await
+    Server::bind(&address)
+        .serve(router().into_make_service())
+        .await
 }
 
-mod axum_bg {
-    use axum::http::StatusCode;
+fn router() -> Router {
+    Router::new().route("/move", get(get_move))
+}
 
-    pub async fn get_move() -> Result<String, (StatusCode, String)> {
-        // Ok("Hello, World".to_string())
-        Err((
-            StatusCode::NOT_IMPLEMENTED,
-            "Will be implemented later on.".to_string(),
-        ))
+#[derive(Serialize)]
+struct ErrorMessage {
+    message: String,
+}
+
+impl ErrorMessage {
+    fn json(message: &str) -> Json<ErrorMessage> {
+        Json(ErrorMessage {
+            message: message.to_string(),
+        })
+    }
+}
+
+async fn get_move() -> Result<String, (StatusCode, Json<ErrorMessage>)> {
+    // Ok("Hello, World".to_string())
+    Err((
+        StatusCode::NOT_IMPLEMENTED,
+        ErrorMessage::json("Will be implemented later on."),
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::router;
+    use axum::http::header::CONTENT_TYPE;
+    use hyper::{Body, Request, StatusCode};
+    use tower::ServiceExt; // for `oneshot
+
+    /// Consumes the response, so use it at the end of the test
+    async fn body_string(response: axum::response::Response) -> String {
+        let body_bytes = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        std::str::from_utf8(&body_bytes).unwrap().to_string()
+    }
+
+    #[tokio::test]
+    async fn get_move_error() {
+        let response = router()
+            .oneshot(Request::builder().uri("/move").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::NOT_IMPLEMENTED);
+        assert_eq!(response.headers()[CONTENT_TYPE], "application/json");
+
+        let body = body_string(response).await;
+        assert_eq!(body, "{\"message\":\"Will be implemented later on.\"}");
     }
 }
