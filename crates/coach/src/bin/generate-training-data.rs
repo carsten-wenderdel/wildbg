780e4aa6285eecf050a436ea9269634970f48f66
Carsten Wenderdel <public@wenderdel.com>
2023-10-21 13:31:18 +0200

    Print more and better information when generating training data
    - new: print number of threads.
    - new: print number of positions.
    - new: print info about finding positions.
    - fixed: calculating remaining time for rollout is only based on rollout times, not on finding positions.
    - fixed: writing each CSV value immediately to file.

--- crates/coach/src/bin/generate-training-data.rs
+++ crates/coach/src/bin/generate-training-data.rs
@@ -1,73 +1,87 @@
 use coach::data::PositionRecord;
 use coach::position_finder::PositionFinder;
 use coach::rollout::RolloutEvaluator;
-use csv::Writer;
 use engine::evaluator::{Evaluator, RandomEvaluator};
 use engine::onnx::OnnxEvaluator;
-use engine::position::{OngoingPhase, Position};
-use engine::probabilities::Probabilities;
+use engine::position::OngoingPhase;
 use std::fs::File;
 use std::io::{stdout, Write};
 use std::time::Instant;
 
-const AMOUNT: usize = 100;
-
+/// This binary is for generating training data in CSV format.
+///
+/// The data is persisted with position ID and the "classic" 5 values for the probabilities.
+/// The resulting file cannot be read by the Python scripts, they have to be converted first with `convert-to-inputs.rs`.
 fn main() -> std::io::Result<()> {
+    // Change the next 4 lines to configure what, how and how much you want to roll out.
     let phase = OngoingPhase::Race;
+    let amount = 10;
+    let rollout_evaluator = OnnxEvaluator::contact_default().map(RolloutEvaluator::with_evaluator);
+    let finder_evaluator = OnnxEvaluator::contact_default();
 
-    let path = format!("training-data/{:?}.csv", phase).to_lowercase();
-    println!("Roll out and write CSV data to {}", path);
-    _ = std::fs::create_dir("training-data");
-    _ = std::fs::remove_file(&path);
-    let mut csv_writer = csv::WriterBuilder::new()
-        .has_headers(false)
-        .from_writer(File::create(&path)?);
-    csv_writer.write_record(PositionRecord::csv_header())?;
-
-    let evaluator = OnnxEvaluator::contact_default().map(RolloutEvaluator::with_evaluator);
-    let finder = OnnxEvaluator::contact_default().map(PositionFinder::with_random_dice);
-    let start = Instant::now();
-
-    match (evaluator, finder) {
-        (Some(evaluator), Some(mut finder)) => {
-            println!("Use onnx evaluator");
-            let positions = finder.find_positions(AMOUNT, phase);
-            for (i, position) in positions.iter().enumerate() {
-                let probabilities = evaluator.eval(position);
-                write_csv_line(&mut csv_writer, position, &probabilities, i, start)?;
-            }
+    match (rollout_evaluator, finder_evaluator) {
+        (Some(rollout_evaluator), Some(finder_evaluator)) => {
+            println!("Use onnx evaluator.");
+            find_and_roll_out(finder_evaluator, rollout_evaluator, amount, phase)?;
         }
         (_, _) => {
-            println!("Couldn't find onnx file, use random evaluator");
-            let evaluator = RolloutEvaluator::with_evaluator(RandomEvaluator {});
-            let mut finder = PositionFinder::with_random_dice(RandomEvaluator {});
-            let positions = finder.find_positions(AMOUNT, phase);
-            for (i, position) in positions.iter().enumerate() {
-                let probabilities = evaluator.eval(position);
-                write_csv_line(&mut csv_writer, position, &probabilities, i, start)?;
-            }
+            println!("Couldn't find neural nets, use random evaluator.");
+            let rollout_evaluator = RolloutEvaluator::with_evaluator(RandomEvaluator {});
+            let finder_evaluator = RandomEvaluator {};
+            find_and_roll_out(finder_evaluator, rollout_evaluator, amount, phase)?;
         }
     }
     println!("\nDone!");
     Ok(())
 }
 
-fn write_csv_line(
-    writer: &mut Writer<File>,
-    position: &Position,
-    probabilities: &Probabilities,
-    i: usize,
-    start: Instant,
+fn find_and_roll_out<T: Evaluator, U: Evaluator>(
+    finder_evaluator: T,
+    rollout_evaluator: U,
+    amount: usize,
+    phase: OngoingPhase,
 ) -> std::io::Result<()> {
-    let record = PositionRecord::new(position, probabilities);
-    writer.serialize(record)?;
+    let path = format!("training-data/{:?}.csv", phase).to_lowercase();
+    _ = std::fs::create_dir("training-data");
+    _ = std::fs::remove_file(&path);
 
-    let done = (i + 1) as f32 / AMOUNT as f32;
+    let mut csv_writer = csv::WriterBuilder::new()
+        .has_headers(false)
+        .from_writer(File::create(&path)?);
+    csv_writer.write_record(PositionRecord::csv_header())?;
+
+    println!(
+        "Find {} '{:?}' positions, roll them out and write data to {}.",
+        amount, phase, path
+    );
+    let find_start = Instant::now();
+    let mut finder = PositionFinder::with_random_dice(finder_evaluator);
+    let positions = finder.find_positions(amount, phase);
+
+    println!(
+        "All positions found in {}. Now performing rollouts on {} threads:",
+        duration(find_start.elapsed().as_secs()),
+        rayon::current_num_threads()
+    );
+
+    let rollout_start = Instant::now();
+    for (i, position) in positions.iter().enumerate() {
+        let probabilities = rollout_evaluator.eval(position);
+        let record = PositionRecord::new(position, &probabilities);
+        csv_writer.serialize(record)?;
+        csv_writer.flush()?;
+        print_progress(i, amount, rollout_start)?;
+    }
+    Ok(())
+}
+
+fn print_progress(i: usize, amount: usize, start: Instant) -> std::io::Result<()> {
+    let done = (i + 1) as f32 / amount as f32;
     let todo = 1.0 - done;
     let seconds_done = start.elapsed().as_secs();
     let seconds_todo = (seconds_done as f32 * (todo / done)) as u64;
     print!(
-        "\rProgress: {:2.2} %. Time elapsed: {}. Time left: {}.",
+        "\rProgress: {:2.2} %. Time elapsed: {}. Time left: {}.  ",
         done * 100.0,
         duration(seconds_done),
         duration(seconds_todo),
