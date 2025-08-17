c75dbfbb025ff38bd51b53a3174c420319a95459
Carsten Wenderdel <public@wenderdel.com>
2025-08-17 13:06:02 +0200

    Remove outdated move generation benches
    We now have better benches, the deleted ones haven't been used lately.

--- crates/benchmarks/benches/move_generation_bench.rs
+++ crates/benchmarks/benches/move_generation_bench.rs
@@ -1,9 +1,7 @@
 use crate::helper::{contact_positions, race_positions};
 use criterion::{criterion_group, criterion_main, Criterion};
-use engine::dice::{Dice, ALL_21};
-use engine::pos;
-use engine::position::GameState::Ongoing;
-use engine::position::{Position, O_BAR, STARTING, X_BAR};
+use engine::dice::Dice;
+use engine::position::Position;
 use mimalloc::MiMalloc;
 use std::hint::black_box;
 
@@ -15,23 +13,6 @@ mod helper;
 // This file contains benchmarks for generating moves/positions for a given position and dice.
 
 // Helper Methods
-
-/// A tuple is returned, `value.0` is the how often `all_positions_after_moving` has been called.
-/// `value.1` is the number of positions returned by that. Not only leave nodes are taken into
-/// account, but all. So for a depth of `0`, the number of positions is already `1`, representing
-/// the initial position.
-fn generations_and_positions(position: &Position, depth: usize) -> (usize, usize) {
-    if depth == 0 || position.game_state() != Ongoing {
-        (0, 1)
-    } else {
-        let result = ALL_21
-            .iter()
-            .flat_map(|(dice, _)| position.all_positions_after_moving(dice))
-            .map(|new_pos: Position| generations_and_positions(&new_pos, depth - 1))
-            .fold((21, 1), |acc, e| (acc.0 + e.0, acc.1 + e.1));
-        result
-    }
-}
 
 fn number_double_moves(positions: &[Position]) -> usize {
     number_moves(positions, &Dice::all_6_double())
@@ -54,63 +35,6 @@ fn number_moves(positions: &[Position], dice: &[Dice]) -> usize {
 }
 
 // Benchmark methods
-
-#[allow(dead_code)]
-fn from_bar(c: &mut Criterion) {
-    let position = pos!(x 1:1, 3:2, 4:3, 5:2, 10:1, 20:2, 23:1, X_BAR:3; o O_BAR: 3, 2:2, 6:1, 17:1, 18:2, 19:3, 21:2, 22:1);
-    let max_depth = 3;
-
-    println!("depth: (# positions, # move generation calls)");
-    for depth in 0..=max_depth {
-        println!(
-            "{}: {:?}",
-            depth,
-            generations_and_positions(&position, depth)
-        );
-    }
-
-    c.bench_function("number_of_generations_and_positions: from_bar", |b| {
-        b.iter(|| generations_and_positions(black_box(&position), black_box(max_depth)))
-    });
-}
-
-#[allow(dead_code)]
-fn bearoff(c: &mut Criterion) {
-    let position = pos!(x 1:4, 3:2, 4:1, 5:5; o 24:4, 23:1, 21:5, 19:3);
-    let max_depth = 2;
-
-    println!("depth: (# positions, # move generation calls)");
-    for depth in 0..=max_depth {
-        println!(
-            "{}: {:?}",
-            depth,
-            generations_and_positions(&position, depth)
-        );
-    }
-
-    c.bench_function("number_of_generations_and_positions: bearoff", |b| {
-        b.iter(|| generations_and_positions(black_box(&position), black_box(max_depth)))
-    });
-}
-
-#[allow(dead_code)]
-fn starting(c: &mut Criterion) {
-    let position = STARTING;
-    let max_depth = 2;
-
-    println!("depth: (# positions, # move generation calls)");
-    for depth in 0..=max_depth {
-        println!(
-            "{}: {:?}",
-            depth,
-            generations_and_positions(&position, depth)
-        );
-    }
-
-    c.bench_function("number_of_generations_and_positions: STARTING", |b| {
-        b.iter(|| generations_and_positions(black_box(&position), black_box(max_depth)))
-    });
-}
 
 #[allow(dead_code)]
 fn contact_double(c: &mut Criterion) {
@@ -166,9 +90,6 @@ fn race_mixed(c: &mut Criterion) {
 
 criterion_group!(
     benches,
-    starting,
-    from_bar,
-    bearoff,
     contact_double,
     race_double,
     contact_mixed,
